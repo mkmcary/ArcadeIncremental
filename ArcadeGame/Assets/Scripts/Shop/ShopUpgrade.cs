6cab6d5dc8264d88a37089ae4e5b569df02a0cba
Michael Costello <mwcostel@ncsu.edu>
2020-12-07 16:17:24 -0500

    Began shop design

---
+++ ArcadeGame/Assets/Scripts/Shop/ShopUpgrade.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopUpgrade : MonoBehaviour
+{
+    public Image image;
+
+    public Text nameText;
+    public Text priceText;
+
+    public string upgradeName;
+    public long upgradePrice;
+
+    public Sprite sprite;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image.sprite = sprite;
+        nameText.text = upgradeName;
+        priceText.text = "" + upgradePrice;
+    }
+}
