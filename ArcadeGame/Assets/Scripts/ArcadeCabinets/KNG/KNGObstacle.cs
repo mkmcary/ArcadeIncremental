1023c82314de7ec84f5770d7891c0264663299d6
mkmcary <mkmcary@gmail.com>
2021-01-05 10:25:07 -0500

    Refined KNG Obstacle movement on platforms.

--- ArcadeGame/Assets/Scripts/ArcadeCabinets/KNG/KNGObstacle.cs
+++ ArcadeGame/Assets/Scripts/ArcadeCabinets/KNG/KNGObstacle.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class KNGObstacle : MonoBehaviour
 {
+    public float obstacleMomentum = 3f;
+    public bool goingLeft;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        goingLeft = false;
     }
 
     // Update is called once per frame
@@ -24,6 +27,34 @@ public class KNGObstacle : MonoBehaviour
             // we hit a kill zone, destroy this object
             Debug.Log("Destroying self.");
             GameObject.Destroy(gameObject);
+        }
+        else
+        {
+            KNGPlatform platform = other.GetComponent<KNGPlatform>();
+
+            if (platform != null)
+            {
+                if (platform.isObstacleFlipper)
+                {
+                    // we hit a platform, flip momentum
+                    Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+
+                    Debug.Log(rb.velocity.x);
+
+                    if (goingLeft)
+                    {
+                        rb.velocity = new Vector2(obstacleMomentum, 0);
+                        goingLeft = false;
+                    }
+                    else
+                    {
+                        rb.velocity = new Vector2(-obstacleMomentum, 0);
+                        goingLeft = true;
+                    }
+                    
+                    rb.angularVelocity = -rb.angularVelocity;
+                }
+            }
         }
     }
 }
--- ArcadeGame/Assets/Scripts/ArcadeCabinets/KNG/KNGPlayerController.cs
+++ ArcadeGame/Assets/Scripts/ArcadeCabinets/KNG/KNGPlayerController.cs
@@ -5,20 +5,28 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class KNGPlayerController : MonoBehaviour
 {
+    // factors affecting player movement
     public float runSpeed = 5f;
     public float jumpForce = 5f;
+    public float climbSpeed = 2f;
+    public float ladderMoveScalar = 0.2f;
 
+    // the player's rigidbody component
     private Rigidbody2D rb;
 
+    // tracks if jumping is allowed
     private bool canJump;
 
+    // ladders
     private KNGLadder currentLadder;
+    private bool isClimbing;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         currentLadder = null;
+        isClimbing = false;
     }
 
     // Update is called once per frame
@@ -32,6 +40,9 @@ public class KNGPlayerController : MonoBehaviour
         else if (Input.GetKey(KeyCode.A))
         {
             rb.velocity = new Vector2(-runSpeed, rb.velocity.y);
+        } else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
         }
 
         // jump
@@ -39,15 +50,53 @@ public class KNGPlayerController : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             canJump = false;
+            isClimbing = false;
         }
 
         // ladder climbing
         if (currentLadder != null)
         {
-            if (Input.GetKeyDown(KeyCode.W))
+            // make sure the player can jump while on a ladder
+            if(!canJump)
             {
-
+                canJump = true;
             }
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                // update if needed
+                if (!isClimbing)
+                {
+                    isClimbing = true;
+                    rb.isKinematic = true;
+                }
+
+                // move up
+                rb.velocity = new Vector2(rb.velocity.x * ladderMoveScalar, climbSpeed);
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                // make sure we arent too low
+                if (transform.position.y > currentLadder.bottomOfLadder.position.y)
+                {
+                    // move down
+                    rb.velocity = new Vector2(rb.velocity.x * ladderMoveScalar, -climbSpeed);
+                } else
+                {
+                    // we are at the bottom, detach
+                    isClimbing = false;
+                }
+            }
+            else if (isClimbing)
+            {
+                rb.velocity = new Vector2(rb.velocity.x * ladderMoveScalar, 0);
+            }
+        }
+
+        // if we arent climbing, act as normal
+        if(!isClimbing) 
+        {
+            rb.isKinematic = false;
         }
     }
 
@@ -60,11 +109,6 @@ public class KNGPlayerController : MonoBehaviour
             // we hit a platform, we can jump
             canJump = true;
         }
-        else if(other.GetComponent<KNGLadder>() != null)
-        {
-            // we are on a ladder, we can climb
-            currentLadder = other.GetComponent<KNGLadder>();   
-        }
         else if(other.GetComponent<KNGObstacle>() != null)
         {
             // we hit an obstacle, game over
@@ -72,9 +116,27 @@ public class KNGPlayerController : MonoBehaviour
         }
     }
 
-    private void OnCollisionExit(Collision collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        Debug.Log("Trigger entered.");
+        KNGLadder ladder = collision.gameObject.GetComponent<KNGLadder>();
+        if (ladder != null)
+        {
+            // we are on a ladder, we can climb
+            currentLadder = ladder;
+            Debug.Log("Attached to ladder.");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        KNGLadder ladder = collision.gameObject.GetComponent<KNGLadder>();
+        if (ladder != null)
+        {
+            // we just left a ladder
+            currentLadder = null;
+            isClimbing = false;
+        }
     }
 
 
