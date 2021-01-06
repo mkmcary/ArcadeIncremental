6132dad89429b57696b614d388571c3eb3589855
Buddwaur <Buddwaur@gmail.com>
2021-01-06 15:14:39 -0500

    Created many prefabs for wall like obstacles

--- ArcadeGame/Assets/Scripts/ArcadeCabinets/BRD/BRDObstacle.cs
+++ ArcadeGame/Assets/Scripts/ArcadeCabinets/BRD/BRDObstacle.cs
@@ -29,6 +29,9 @@ public class BRDObstacle : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
+        if(collision.gameObject.GetComponent<BRDPlayerController>() != null)
+        {
+            Debug.Log("Hit player");
+        }
     }
 }
--- ArcadeGame/Assets/Scripts/ArcadeCabinets/BRD/BRDObstacleSpawner.cs
+++ ArcadeGame/Assets/Scripts/ArcadeCabinets/BRD/BRDObstacleSpawner.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class BRDObstacleSpawner : MonoBehaviour
@@ -9,79 +8,67 @@ public class BRDObstacleSpawner : MonoBehaviour
     public Transform ceilingSpawn;
 
     // Obstacle Prefabs
-    public GameObject spikeObstacle;
-    public GameObject wallObstacle;
+    public List<GameObject> spikeObstacles;
+    public List<GameObject> wallObstacles;
+    public List<GameObject> topSpikedWallObstacles;
 
     private float timer = 0;
-    private float interval = 5f;
+    private float interval = 3f;
 
     private void FixedUpdate()
     {
         timer += Time.fixedDeltaTime;
-        if(timer >= interval)
+        if (timer >= interval)
         {
-            int coinFlip = Random.Range(0, 2);
-            if(coinFlip == 0)
+            int topOrBottomRoll = Random.Range(0, 2);
+            if (topOrBottomRoll == 0)
             {
-                StartCoroutine(SpawnObstacles(groundSpawn.position, false));
+                //StartCoroutine(SpawnObstacles(groundSpawn.position, false));
+                SpawnObstacle(groundSpawn.position);
+            } else if (topOrBottomRoll == 1)
+            {
+                //StartCoroutine(SpawnObstacles(ceilingSpawn.position, true));
+                SpawnObstacle(ceilingSpawn.position);
             } else
             {
-                StartCoroutine(SpawnObstacles(ceilingSpawn.position, true));
+                SpawnObstacle(groundSpawn.position);
+                SpawnObstacle(ceilingSpawn.position);
             }
-            
+
             timer = 0;
         }
     }
 
-    private IEnumerator SpawnObstacles(Vector3 location, bool flipped)
+    private void SpawnObstacle(Vector3 location)
     {
+        bool flipped = location == ceilingSpawn.position;
+
         int obstacleSelector = Random.Range(0, 4);
-        if(obstacleSelector == 0)
+        if (obstacleSelector == 0)
         {
             // Creates a spike on either floor or ceiling
-            int numberOfSpikes = Random.Range(1, 10);
-            for (int i = 0; i < numberOfSpikes; i++)
-            {
-                SpawnSpike(location, flipped);
-                yield return new WaitForSeconds(0.2f);
-            }
+            SpawnSpike(location, flipped);
         }
-        else if(obstacleSelector == 1)
+        else if (obstacleSelector == 1)
         {
             // Creates a ceiling and ground spike
-            int numberOfTopSpikes = Random.Range(1, 10);
-            int numberOfBottomSpikes = Random.Range(1, 10);
-            int maxSpikes = Mathf.Max(numberOfTopSpikes, numberOfBottomSpikes);
-            int offsetSpikes = Random.Range(0,maxSpikes);
-            int topOffset = 0;
-            int botOffset = 0;
-            if (flipped)
-            {
-                numberOfBottomSpikes -= offsetSpikes;
-                botOffset = offsetSpikes;
-            } else
-            {
-                numberOfTopSpikes -= offsetSpikes;
-                topOffset = offsetSpikes;
-            }
-            for (int i = 0; i < maxSpikes; i++)
-            {
-                if(i < numberOfTopSpikes && i >= topOffset)
-                {
-                    SpawnSpike(ceilingSpawn.position, true);
-                }
-                if(i < numberOfBottomSpikes && i >= botOffset)
-                {
-                    SpawnSpike(groundSpawn.position, false);
-                }
-                yield return new WaitForSeconds(0.2f);
-            }
+            SpawnCeilingAndGroundSpike(location, flipped);
+        } 
+        else if (obstacleSelector == 2)
+        {
+            SpawnWall(location, flipped);
+        }
+        else if(obstacleSelector == 3)
+        {
+            SpawnTopSpikedWall(location, flipped);
         }
     }
 
+
     private void SpawnSpike(Vector3 location, bool flipped)
     {
-        GameObject spike = Instantiate(spikeObstacle, location, Quaternion.identity);
+        int length = Random.Range(0, spikeObstacles.Count);
+        GameObject spike = Instantiate(spikeObstacles[length], location, Quaternion.identity);
         if (flipped)
         {
             Vector2 vTemp = spike.transform.localScale;
@@ -90,8 +77,48 @@ public class BRDObstacleSpawner : MonoBehaviour
         }
     }
 
+    private void SpawnCeilingAndGroundSpike(Vector3 location, bool flipped)
+    {
+        int offsetSpikes = Random.Range(0, 5);
+        float topOffset = 0;
+        float botOffset = 0;
+        if (flipped)
+        {
+            botOffset = offsetSpikes;
+        }
+        else
+        {
+            topOffset = offsetSpikes;
+        }
+        Vector3 topLoc = ceilingSpawn.position;
+        topLoc.x += topOffset;
+        Vector3 botLoc = groundSpawn.position;
+        botLoc.x += botOffset;
+        SpawnSpike(topLoc, true);
+        SpawnSpike(botLoc, false);
+    }
+
     private void SpawnWall(Vector3 location, bool flipped)
     {
+        int index = Random.Range(0, wallObstacles.Count);
+        GameObject wall = Instantiate(wallObstacles[index], location, Quaternion.identity);
+        if (flipped)
+        {
+            Vector2 vTemp = wall.transform.localScale;
+            vTemp.y *= -1;
+            wall.transform.localScale = vTemp;
+        }
+    }
 
+    private void SpawnTopSpikedWall(Vector3 location, bool flipped)
+    {
+        int index = Random.Range(0, topSpikedWallObstacles.Count);
+        GameObject spikedWall = Instantiate(topSpikedWallObstacles[index], location, Quaternion.identity);
+        if (flipped)
+        {
+            Vector2 vTemp = spikedWall.transform.localScale;
+            vTemp.y *= -1;
+            spikedWall.transform.localScale = vTemp;
+        }
     }
 }
