d9f6409c569456765f6bab781544744f5fc586d8
mkmcary <mkmcary@gmail.com>
2021-01-15 15:22:40 -0500

    Began work on SPC  Arcade Game.

---
+++ ArcadeGame/Assets/Scripts/ArcadeCabinets/SPC/GameScripts/SPCPlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class SPCPlayerController : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 leftVelocity;
+    private Vector2 rightVelocity;
+
+    public float fireInterval = 0.2f;
+    public bool shouldFire;
+    private bool isFiring;
+    public GameObject projectileToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        leftVelocity = new Vector2(-movementSpeed, 0f);
+        rightVelocity = new Vector2(movementSpeed, 0f);
+        shouldFire = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // left and right
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = rightVelocity;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = leftVelocity;
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+
+        // fire
+        if(Input.GetKeyDown(KeyCode.Space) && !isFiring)
+        {
+            StartCoroutine(Fire());
+        } 
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            shouldFire = false;
+        }
+
+    }
+
+    private IEnumerator Fire()
+    {
+        shouldFire = true;
+        isFiring = true;
+        while (shouldFire) {
+            // spawn
+            GameObject projectile = Instantiate(projectileToSpawn, transform.position, Quaternion.identity);
+            projectile.GetComponent<SPCProjectile>().StartMoving(5f, new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)));
+
+            // wait
+            yield return new WaitForSeconds(fireInterval);
+        }
+        isFiring = false;
+    }
+}
---
+++ ArcadeGame/Assets/Scripts/ArcadeCabinets/SPC/GameScripts/SPCProjectile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SPCProjectile : MonoBehaviour
+{
+    public float moveTime = 5f;
+    private bool isMoving = false;
+    private float timer = 0f;
+
+    private void FixedUpdate()
+    {
+        if(isMoving)
+        {
+            timer += Time.fixedDeltaTime;
+            if(timer >= moveTime)
+            {
+                GameObject.Destroy(gameObject);
+            }
+        }
+    }
+
+    public void StartMoving(float movementSpeed, Color color)
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0f, movementSpeed);
+        GetComponent<SpriteRenderer>().color = color;
+
+        isMoving = true;
+    }
+}
