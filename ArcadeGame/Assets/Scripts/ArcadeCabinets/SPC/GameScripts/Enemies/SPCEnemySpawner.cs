3651720ebc2fcea5c4649c7abcfabf2c26c89869
mkmcary <mkmcary@gmail.com>
2021-02-06 14:42:44 -0500

    Created enemy prefabs, added enemy sprites.

---
+++ ArcadeGame/Assets/Scripts/ArcadeCabinets/SPC/GameScripts/Enemies/SPCEnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SPCEnemySpawner : MonoBehaviour
+{
+    private bool waveComplete = true;
+
+    public GameObject stdTroopPrefab;
+    public GameObject hvyTroopPrefab;
+    public GameObject snpTroopPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(waveComplete)
+        {
+            SpawnNewWave();
+        }
+    }
+
+    private void SpawnNewWave()
+    {
+
+    }
+}
